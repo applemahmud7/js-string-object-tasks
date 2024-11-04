9cec3f3e19ce5f3258dc4f079dd43c8504dc646e
Apple Mahmud <applemahmud.primary@gmail.com>
2024-11-04 18:09:20 +0600

    second commit

---
+++ concat.js
@@ -0,0 +1,12 @@
+const first = 'Abid';
+const last = 'Navid';
+
+
+const fullName = first + ' ' + last;
+
+const fullName2 = first.concat(' ').concat(last)
+console.log(fullName);
+console.log(fullName2);
+
+
+console.log(last.includes('a'))
---
+++ declare.js
@@ -0,0 +1,8 @@
+const pen = { brand: 'econo', price: 10, color: 'black' }
+const pencil = new Object()
+pencil.brand = 'high school';
+pencil.price = 15;
+color = 'Red'
+console.log(pencil)
+const rubber = Object.create({})
+console.log(rubber)
