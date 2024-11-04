5e67187d1a0cb9be4eef8f530c2fc47066108bf0
Apple Mahmud <applemahmud.primary@gmail.com>
2024-11-04 18:55:55 +0600

    loop added

---
+++ loop.js
@@ -0,0 +1,15 @@
+const mobile = {
+    brand: 'samsung',
+    price: 25000,
+    color: 'black',
+    camera: '12mp',
+    isNew: true
+}
+
+
+const keys = Object.keys(mobile);
+
+   for (const lulu of keys){
+    console.log(lulu, ':' , mobile[lulu]);
+   }
+   
